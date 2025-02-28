721d7853d5c06061eddcea7811c90f7a17dcb1c1
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-02-28 19:38:02 +0530

    28feb

--- Factorial_by_recursion.java
+++ Factorial_by_recursion.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
 public class Factorial_by_recursion {
-    public static long facte = 1;
     public static long fact(int num)
     {
         if(num == 0)
         {
-            return facte;
+            return 1;
         }
-        facte *= num;
-        return fact(--num);
+        return num * fact(num-1);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
