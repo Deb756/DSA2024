05742c84d2b26515d1d311d679490fc0d346004b
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-02-28 07:42:41 +0530

    28feb

---
+++ plus_one.java
@@ -0,0 +1,22 @@
+// Given a non-negative number represented as a list of digits, add 1 to the number (increment the number represented by the digits). The digits are stored such that the most significant digit is first element of array.
+// Example 1:
+// Input: 
+// N = 3
+// arr[] = {1, 2, 4}
+// Output: 
+// 1 2 5
+// Explanation:
+// 124+1 = 125, and so the Output
+// Input: 
+// N = 3
+// arr[] = {9,9,9}
+// Output: 
+// 1 0 0 0
+// Explanation:
+// 999+1 = 1000, and so the output
+
+public class plus_one {
+    public static void main(String[] args) {
+        
+    }
+}
