37cc7368d22f77d063dafd443804578453a26ea2
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-02 07:43:22 +0530

    02mar

---
+++ Palindrom_no_in_a_range.java
@@ -0,0 +1,15 @@
+// Checking for Palindromic Numbers in a Range
+// Difficulty: Medium
+// Topics: Mathematical Computations
+// Description: Write a program to check for palindromic numbers within a given range.
+// Example:
+// Input: start = 1, end = 100
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99]
+// Explanation: Palindromic numbers between 1 and 100 are the numbers listed.
+
+
+public class Palindrom_no_in_a_range {
+    public static void main(String[] args) {
+        
+    }
+}
