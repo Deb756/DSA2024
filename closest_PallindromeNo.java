bb0174e8e9e19586f72cb408034f1f5555c167d2
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-04 08:17:01 +0530

    04mar

---
+++ closest_PallindromeNo.java
@@ -0,0 +1,22 @@
+// Given a number num, our task is to find the closest Palindrome number whose absolute difference with the given number is minimal. If 2 Palindrome numbers have the same absolute difference as the given number, take the smaller one.
+
+// Example 1:
+
+// Input: num = 9
+// Output: 9
+// Explanation: 9 itself is a palindrome
+// number.
+// Example 2:
+
+// Input: num = 489
+// Output: 484
+// Explanation: closest palindrome numbers from
+// 489 are 484 and 494. Absolute difference between
+// 489 and 494 is equal to the absolute difference
+// between 484 and 489 but 484 is smaller than 494.
+
+public class closest_PallindromeNo {
+    public static void main(String[] args) {
+
+    }
+}
