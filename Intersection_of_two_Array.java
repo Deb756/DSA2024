b1fef225f779c61ea1a1315b55b885eb8ae8d3d4
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-09 11:53:21 +0530

    09mar

--- Intersection_of_two_Array.java
+++ Intersection_of_two_Array.java
@@ -14,18 +14,34 @@
 // Output: 2
 // Explanation: 20 and 30 are the elements in the intersection of the two arrays.
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Intersection_of_two_Array {
     public static int numberofElementsInIntersection(int a[], int b[]) {
         // Your code here
-        return 0;
+        // using hash set contains()
+        int count = 0;
+        Set<Integer> set = new HashSet<>();
+        for (Integer elem : a) {
+            set.add(elem);
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            if (set.contains(b[i])) {
+                count++;
+            }
+        }
+
+        // System.out.println(set);
+        return count;
     }
+
     public static void main(String[] args) {
-        int a[] = {89, 24, 75, 11, 23};
-        int b[] = {89, 2, 4};
-        Arrays.sort(a);
-        Arrays.sort(b);
-        System.out.println("Number of Elem Intersection : "+numberofElementsInIntersection(a, b));
+        int a[] = { 20, 10, 30, 50, 40 };
+        int b[] = { 15, 25, 30, 20, 35 };
+        // Arrays.sort(a);
+        // Arrays.sort(b);
+        System.out.println("Number of Elem Intersection : " + numberofElementsInIntersection(a, b));
     }
 }
