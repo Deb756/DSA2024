2b3c1a77115271d53d5b275b0db889ea35dd8d34
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-12 07:28:04 +0530

    12mar

---
+++ MST_prims_algorithm.java
@@ -0,0 +1,29 @@
+// Given a weighted, undirected, and connected graph with V vertices and E edges, your task is to find the sum of the weights of the edges in the Minimum Spanning Tree (MST) of the graph. The graph is represented by an adjacency list, where each element adj[i] is a vector containing vector of integers. Each vector represents an edge, with the first integer denoting the endpoint of the edge and the second integer denoting the weight of the edge.
+// Input:
+// 3 3
+// 0 1 5
+// 1 2 3
+// 0 2 1
+// Output: 4
+// Explanation: 
+// The Spanning Tree resulting in a weight
+// of 4 is shown above.
+// Input: 
+// 2 1
+// 0 1 5
+// Output: 5 
+// Explanation: Only one Spanning Tree is possible which has a weight of 5.
+// (GFG) (check here for better explanation visually)
+
+import java.util.List;
+
+public class MST_prims_algorithm {
+    public static int spanningTree(int V, int E, List<List<int[]>> adj) {
+        // Code Here.
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
