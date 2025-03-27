8872f425e56f1267c1f05f47e5cba293c19eab41
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-27 07:01:17 +0530

    27mar

---
+++ CaseSpecificSortingofStrings.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+// import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CaseSpecificSortingofStrings {
+    public static String sortedStr(String s)
+    {   
+        List<Character> upper = new ArrayList<>();
+        List<Character> lower = new ArrayList<>();
+        for(char ch : s.toCharArray())
+        {
+            if(Character.isUpperCase(ch))
+            {
+                upper.add(ch);
+            }
+            else
+            {
+                lower.add(ch);
+            }
+        }
+        Collections.sort(upper);
+        Collections.sort(lower);
+        StringBuilder sb = new StringBuilder();
+        int i=0;
+        int j=0;
+        for(char ch:s.toCharArray())
+        {
+            if(Character.isUpperCase(ch))
+            {
+                sb.append(upper.get(i++));
+            }
+            else
+            {
+                sb.append(lower.get(j++));
+            }
+        }
+
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String s = "defRTSersUXI";
+        System.out.println(sortedStr(s));
+    }
+}
