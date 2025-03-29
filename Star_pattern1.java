13c44e987c0fd4fb5ebf426967f69c6be1032691
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-29 07:19:31 +0530

    29mar

--- Star_pattern1.java
+++ Star_pattern1.java
@@ -1,7 +1,7 @@
 public class Star_pattern1 {
     public static void main(String[] args) {
 
-        int row = 4;
+        int row = 9;
         int star = 1;
         for (int i = 0; i < row; i++) {
             for (int j = i + 1; j < row; j++) {
@@ -12,6 +12,21 @@ public class Star_pattern1 {
             }
             System.out.println();
             star += 2;
+        }
+        int row2 = row;
+        int star2 = star-2;
+        int space = 0;
+        for (int i = star; i >= row2; i--) {
+            for(int j = 0;j<space;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k = star2; k > 0; k--) {
+                    System.out.print("*");
+            }
+            System.out.println();
+            star2 -= 2;
+            space +=1;
         }
     }
 }
