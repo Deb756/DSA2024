54a125e31fd871a77a3d1f171ea2f0ce8d37aa85
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-03-31 07:09:00 +0530

    31mar

--- SubarrayswithsumK.java
+++ SubarrayswithsumK.java
@@ -13,13 +13,35 @@
 // Explaination: No subarray with 0 sum.
 // Time Complexity: O(n)Auxiliary Space: O(n)
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarrayswithsumK {
     public static int countSubarrays(int arr[], int k) {
-        return 0;
+        // your code here
+        // taking prefix sum in map
+        Map<Integer,Integer> prefixSum = new HashMap<>();
+        int res = 0;
+        int currSum = 0;
+        for(int i=0;i<arr.length;i++)
+        {
+            currSum += arr[i];
+            if(currSum == k)
+            {
+                res++;
+            }
+            if(prefixSum.containsKey(currSum-k))
+            {
+                res += prefixSum.get(currSum-k);
+            }
+            prefixSum.put(currSum,prefixSum.getOrDefault(currSum, 0)+1);
+        }
+
+        return res;
     }
     public static void main(String[] args) {
-        int arr[] = {10, 2, -2, -20, 10};
-        int k = -10;
+        int arr[] = {9, 4, 20, 3, 10, 5};
+        int k = 13;
         System.out.println(countSubarrays(arr, k));
     }
 }
