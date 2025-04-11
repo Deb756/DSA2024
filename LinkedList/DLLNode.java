3236ff034f876400952cd08336c5823c53430d62
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-04-11 07:39:13 +0530

    11Apr

---
+++ LinkedList/DLLNode.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class DLLNode {
+    int data;
+    Node next;
+    Node prev;
+
+    DLLNode(int data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
