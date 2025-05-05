d4b6bd145d6d2b3259bbb30b4748d5ba886d69da
Debabrat <123225627+Deb756@users.noreply.github.com>
2025-05-05 21:20:15 +0530

    5may

--- Stack/StackArr.java
+++ Stack/StackArr.java
@@ -1,5 +1,46 @@
 package Stack;
 
-public class StackArr {
+class StackN {
+    int size;
+    int top;
+    int arr[];
+}
 
+public class StackArr {
+    private StackN head;
+
+    public boolean isEmpty() {
+        if (head.top == -1)
+            return true;
+        return false;
+    }
+
+    public boolean isFull() {
+        if (head.top == head.size - 1)
+            return true;
+        return false;
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            System.out.println("Stack is Full ");
+            return;
+        }
+        head.top++;
+        head.arr[head.top] = data;
+    }
+
+    public int pack(int i) {
+        int idx = head.top - i + 1;
+        if (idx < 0)
+            return -1;
+        else {
+            return head.arr[idx];
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
